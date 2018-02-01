ad2432611501782011dd210d68c60ff3a7c770af
Trevor Sherk <trevorsherk@gmail.com>
2018-02-01 17:17:28 -0500

    Removed print statement.

--- djangoip/ipapp/forms.py
+++ djangoip/ipapp/forms.py
@@ -5,9 +5,7 @@ from django.template.defaultfilters import striptags
 class IpForm(forms.Form):
     ip = forms.GenericIPAddressField()
 
-
     def clean_ip(self):
-        print("got here")
         ip = self.cleaned_data['ip']
         if striptags(ip) != ip:
             raise forms.ValidationError("You have some HTML code in there.")
