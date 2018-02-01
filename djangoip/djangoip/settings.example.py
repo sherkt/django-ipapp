7f53dd6e0a0c46b9fa36ef80c69021eae01787e4
Trevor Sherk <trevorsherk@gmail.com>
2018-02-01 15:31:25 -0500

    Updated settings example to include app and redis cache.

--- djangoip/djangoip/settings.example.py
+++ djangoip/djangoip/settings.example.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ipapp',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
 
