cf534d6536c1e303faeff7dee5d6cf808ad5c62b
Trevor Sherk <trevorsherk@gmail.com>
2018-02-01 18:50:54 -0500

    Added more fields to admin area.

--- djangoip/ipapp/admin.py
+++ djangoip/ipapp/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import GeoIP
 
 class GeoIPAdmin(admin.ModelAdmin):
-    list_display = ['ip_address', 'city', ]
+    list_display = ['ip_address', 'city', 'country_code', 'last_activity', ]
     readonly_fields = ['date_created', 'last_activity', ]
 admin.site.register(GeoIP, GeoIPAdmin)
