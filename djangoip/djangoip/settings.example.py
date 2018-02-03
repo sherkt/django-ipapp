3e808b6fa72deed6ca962bfbbf1c9059c80db797
Trevor Sherk <trevorsherk@gmail.com>
2018-02-02 20:14:33 -0500

    Added a new template directory for 404 and 500 pages.

--- djangoip/djangoip/settings.example.py
+++ djangoip/djangoip/settings.example.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'djangoip.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'djangoip/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- djangoip/djangoip/urls.py
+++ djangoip/djangoip/urls.py
@@ -4,9 +4,6 @@ from django.urls import path
 from ipapp import views
 
 
-handler404 = views.page_not_found
-handler500 = views.server_error
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
