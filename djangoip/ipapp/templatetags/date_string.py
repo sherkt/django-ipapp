371bf040c801be2df5135a1b5c8b0e011d9f15c0
Trevor Sherk <trevorsherk@gmail.com>
2018-02-02 22:12:14 -0500

    Added get_unix_string to date_string template tag.

--- djangoip/ipapp/templatetags/date_string.py
+++ djangoip/ipapp/templatetags/date_string.py
@@ -4,16 +4,26 @@ import pytz
 from django.template import Library
 
 
-def date_string(value):
+def date_string(value, arg=None):
     utc = pytz.utc
     if isinstance(value, str):
         try:
-            value = datetime.strptime(value[:-1], "%Y-%m-%dT%H:%M:%S")
+            value = datetime.strptime(value[:-1], arg)
             value = value.replace(tzinfo=utc)
         except ValueError:
             pass
     return value
 
+def utc_unix_string(value):
+    utc = pytz.utc
+    try:
+        value = datetime.utcfromtimestamp(value)
+        value = value.replace(tzinfo=utc)
+    except ValueError:
+        pass
+    return value
+
 
 register = Library()
 register.filter('date_string', date_string)
+register.filter('utc_unix_string', utc_unix_string)
